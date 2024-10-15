054d2d3541ee57160a1bfc9f461b0015147252ac
Stefano Zanetti Azzi <marciostefanoazzi2@gmail.com>
2024-10-15 09:22:26 -0300

    Aula 15/10

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
         'message' => 'OK'
+    ]);
+});
+
+Route::get('/somar', function(Request $request) {
+    // Não está chegando nada pela request
+    if (count($request->all()) < 1){
+        return response()->json([
+            'message' => 'Não há valores para somar.',
+        ], 406);
+    }
+    $soma = array_sum($request->all());
+    return response()->json([
+        'message'=> 'Somado com sucesso',
+        'sum' => $soma,
     ]);
 });
