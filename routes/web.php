be1367f3176e304a95af407b3d1eb3f6574606c3
Stefano Zanetti Azzi <76130321+Stefanokkk@users.noreply.github.com>
2024-10-25 11:37:40 -0300

    salva

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,10 @@ Route::get('/somar', function(Request $request) {
         'sum' => $soma,
     ]);
 });
+
+Route::prefix('/people')->group(function() {
+    Route::get('/list', [PeopleController::class, 'list']);
+
+    Route::post('/store', [PeopleController::class, 'store']);
+});
+
